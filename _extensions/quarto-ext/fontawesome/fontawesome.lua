384cb478eba3bdc306b3a2c35a7f18d1069d84b4
rameliaz <Amelia.Zein@cais-research.de>
2026-05-11 10:01:52 +0200

    fixing cov between ri and rs

---
+++ _extensions/quarto-ext/fontawesome/fontawesome.lua
@@ -0,0 +1,98 @@
+local function ensureLatexDeps()
+  quarto.doc.use_latex_package("fontawesome6")
+end
+
+local fontawesome_cdn_loaded = false
+
+local function ensureHtmlDeps()
+  quarto.doc.add_html_dependency({
+    name = 'fontawesome6',
+    version = '6.7.2',
+    stylesheets = {'assets/css/latex-fontsize.css'}
+  })
+  if not fontawesome_cdn_loaded then
+    fontawesome_cdn_loaded = true
+    quarto.doc.include_text(
+      'in-header',
+      '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />\n'
+    )
+  end
+end
+
+local function isEmpty(s)
+  return s == nil or s == ''
+end
+
+local function isValidSize(size)
+  local validSizes = {
+    "tiny",
+    "scriptsize",
+    "footnotesize",
+    "small",
+    "normalsize",
+    "large",
+    "Large",
+    "LARGE",
+    "huge",
+    "Huge"
+  }
+  for _, v in ipairs(validSizes) do
+    if v == size then
+      return size
+    end
+  end
+  return ""
+end
+
+return {
+  ["fa"] = function(args, kwargs)
+
+    local group = "solid"
+    local icon = pandoc.utils.stringify(args[1])
+    if #args > 1 then
+      group = icon
+      icon = pandoc.utils.stringify(args[2])
+    end
+    
+    local title = pandoc.utils.stringify(kwargs["title"])
+    if not isEmpty(title) then
+      title = " title=\"" .. title  .. "\""
+    end
+
+    local label = pandoc.utils.stringify(kwargs["label"])
+    if isEmpty(label) then
+      label = " aria-label=\"" .. icon  .. "\""
+    else
+      label = " aria-label=\"" .. label  .. "\""
+    end
+
+    local size = pandoc.utils.stringify(kwargs["size"])
+    
+    -- detect html (excluding epub which won't handle fa)
+    if quarto.doc.is_format("html:js") then
+      ensureHtmlDeps()
+      if not isEmpty(size) then
+        size = " fa-" .. size
+      end
+      return pandoc.RawInline(
+        'html',
+        "<i class=\"fa-" .. group .. " fa-" .. icon .. size .. "\"" .. title .. label .. "></i>"
+      )
+    -- detect pdf / beamer / latex / etc
+    elseif quarto.doc.is_format("pdf") then
+      ensureLatexDeps()
+      local options = ""
+      if (group == "regular") then
+        options = "[regular]"
+      end
+      local icons = "\\faIcon" .. options .. "{" .. icon .. "}"
+      if isEmpty(isValidSize(size)) then
+        return pandoc.RawInline('tex', icons)
+      else
+        return pandoc.RawInline('tex', "{\\" .. size .. icons .. "}")
+      end
+    else
+      return pandoc.Null()
+    end
+  end
+}
